3dadc276199bd6bf10f8e220d6298fa20dc7e760
Dunia Goncalves <dgoncalves@everquote.com>
2019-02-04 12:57:54 -0500

    Learn and practice using the redux store

--- expensify-app/src/playground/redux-101.js
+++ expensify-app/src/playground/redux-101.js
@@ -1,7 +1,68 @@
 import {  createStore } from 'redux';
 
-const store = createStore((state = { count: 0 }) => {
-  return state;
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type){
+    case 'INCREMENT':
+    const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+      return {
+        count: state.count + incrementBy
+      };
+    case 'DECREMENT':
+    const decrementBy = typeof action.decrementBy == 'number' ? action. decrementBy : 1;
+      return {
+        count: state.count - decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+     return state
+  }
 });
 
-console.log(store.getState());
+const unsubsribe = store.subscribe(() => {
+  console.log(store.getState());
+});
+
+// Actions - than an object that gets sent to the store
+
+// I'd like to increment the count
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5
+});
+
+store.dispatch({
+  type: 'INCREMENT'
+});
+
+// I'd like to decrement the count
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+// I'd like to set the count to 0
+store.dispatch({
+  type: 'RESET'
+});
+
+store.dispatch({
+  type: 'DECREMENT'
+});
+
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 6
+});
+
+store.dispatch({
+  type: 'SET',
+  count: 101
+});
+
+//Actions are a way to communicate to the store
