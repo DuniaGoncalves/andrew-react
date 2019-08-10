e8d20922fd4574a4217854680bd8d73964dd5409
Dunia Goncalves <dgoncalves@everquote.com>
2019-08-10 19:02:30 -0400

    connecting store and component with react-redux

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRouter from './router/AppRouter';
 import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import { addExpense } from './actions/expenses';
 import { setTextFilter } from './actions/filters';
 import getVisibleExpenses from './selectors/expenses';
@@ -13,9 +14,20 @@ store.dispatch(addExpense({ description: 'Water Bill' }));
 store.dispatch(addExpense({ description: 'Gas Bill' }));
 store.dispatch(setTextFilter('water'));
 
+setTimeout(() => {
+  store.dispatch(setTextFilter('rent'));
+}, 3000)
+
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 console.log(visibleExpenses);
 
 console.log(store.getState());
-ReactDOM.render(<AppRouter />, document.getElementById('app'))
+
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById('app'))
--- expensify-app/src/components/ExpenseDashboardPage.js
+++ expensify-app/src/components/ExpenseDashboardPage.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import ExpenseList from './ExpenseList'
 
 const ExpenseDashBoardPage = () => (
-  <div>This is from Dashbboard</div>
+  <ExpenseList />
 );
 
 export default ExpenseDashBoardPage;
