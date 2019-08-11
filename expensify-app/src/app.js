537bdc347942acf5643fc7138502f816fd502beb
Dunia Goncalves <dgoncalves@everquote.com>
2019-08-10 23:12:09 -0400

    dropdown for picking sortby

--- expensify-app/src/app.js
+++ expensify-app/src/app.js
@@ -10,13 +10,9 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water Bill' }));
-store.dispatch(addExpense({ description: 'Gas Bill' }));
-store.dispatch(setTextFilter('water'));
-
-setTimeout(() => {
-  store.dispatch(setTextFilter('bill'));
-}, 3000)
+store.dispatch(addExpense({ description: 'Water Bill', amount:4500 }));
+store.dispatch(addExpense({ description: 'Gas Bill', createdAt: 1000 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 109500 }));
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
